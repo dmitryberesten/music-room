8102644f854fb29a336590613314e6725d3e6d3b
Dima <dmitryberesten@gmail.com>
2022-08-30 20:58:53 +0300

    Додав картинки товару

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'Gibson Les Paul',
+        img: 'https://i.ibb.co/p3T9Zq5/2018-explorer-antique-natural-1.jpg',
+        price: 25000,
+    },
+    {
+        id: 'el2',
+        name: 'Gibson Les Paul',
+        img: 'https://i.ibb.co/p3T9Zq5/2018-explorer-antique-natural-1.jpg',
+        price: 25000,
+    },
+    {
+        id: 'el3',
+        name: 'Gibson Les Paul',
+        img: 'https://i.ibb.co/p3T9Zq5/2018-explorer-antique-natural-1.jpg',
+        price: 25000,
+    },
+]
