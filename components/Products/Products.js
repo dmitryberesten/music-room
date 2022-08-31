78ff625eb71e317f33a79932f453186c3494ea22
Dima <dmitryberesten@gmail.com>
2022-08-31 19:43:55 +0300

    Додав на сторінку товари

---
+++ components/Products/Products.js
@@ -0,0 +1,27 @@
+class Products {
+    render() {
+        let htmlCatalog = '';
+
+        CATALOG.forEach(({ id, name, price, img }) => {
+            htmlCatalog += `
+            <li>
+                <span>${name}</span>
+                <img src="${img}" />
+                <span>${price}</span>
+                <button>Добавить в корзину</button>
+            </li>
+            `;
+        });
+
+        const html = `
+        <ul>
+            ${htmlCatalog}
+        </ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
