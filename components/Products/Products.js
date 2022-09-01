bb7d0bbf1f13f3a8982465c6c2b921a59b9e3c2c
Dima <dmitryberesten@gmail.com>
2022-09-01 22:58:08 +0300

    Додав градієнти, тіні, іконки

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -4,17 +4,17 @@ class Products {
 
         CATALOG.forEach(({ id, name, price, img }) => {
             htmlCatalog += `
-            <li>
-                <span>${name}</span>
-                <img src="${img}" />
-                <span>${price}</span>
-                <button>Добавить в корзину</button>
+            <li class="products-element">
+                <span class="products-element__name">${name}</span>
+                <img class="products-element__img" src="${img}" />
+                <span class="products-element__price">💰 ${price.toLocaleString()} USD</span>
+                <button class="products-element__btn">Добавить в корзину</button>
             </li>
             `;
         });
 
         const html = `
-        <ul>
+        <ul class="products-container">
             ${htmlCatalog}
         </ul>
         `;
