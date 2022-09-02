86cdf238b51418c920fb6f039b9e3f06555b8a81
Dima <dmitryberesten@gmail.com>
2022-09-02 22:29:16 +0300

    Update localStorageUtil.js

---
+++ utils/localStorageUtil.js
@@ -0,0 +1,33 @@
+class LocalStorageUtil { 
+    constructor() { 
+        this.keyName = 'products';
+    }
+
+    getProducts() { 
+        const productsLocalStorage = localStorage.getItem(this.keyName);
+        if (productsLocalStorage !== null) { 
+            return JSON.parse(productsLocalStorage);
+        }
+        return [];
+    }
+
+    putProducts(id) { 
+        let products = this.getProducts();
+        let pushProducts = false;
+        const index = products.indexOf(id);
+
+        if (index === -1) {
+            products.push(id);
+            pushProducts = true;
+        } else { 
+            products.splice(index, 1);
+        }
+
+        
+        localStorage.setItem(this.keyName, JSON.stringify(products));
+
+        return { pushProducts, products }
+    }
+}
+
+const localStorageUtil = new LocalStorageUtil();
