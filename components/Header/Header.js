ccd5b7252bf4ffc8b434c4ce6c0f9b878aefd3ef
Dima <dmitryberesten@gmail.com>
2022-09-06 23:39:59 +0300

    Додав стилізацію кошика

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,13 @@
 class Header { 
+    handlerOpenShoppingPage() { 
+        shoppingPage.render();
+    }
 
     render(count) { 
         const html = `
             <div class="header-container">
-                <div class="header-counter">🗑️ ${count}</div>
+                <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
+                🗑️ ${count}</div>
             </div>
         `
         ROOT_HEADER.innerHTML = html;
