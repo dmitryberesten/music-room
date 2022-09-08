0458ee35e4332940b0bc2ea24f0b5d193e7db548
Dima <dmitryberesten@gmail.com>
2022-09-08 21:35:02 +0300

    Додав прелоадер

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ function render() {
     productsPage.render();
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
-
-
 
 fetch('server/catalog.json')
     .then(res => res.json())
     .then(body => { 
         CATALOG = body;
-        render();
+
+        setTimeout(() => {
+            spinnerPage.handleClear();
+            render();
+        }, 1000);
+
+       
     })
     .catch(error => { 
         console.log(error);
