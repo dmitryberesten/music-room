1dcabf8b490304a42e4b773967e7cf8caa789801
Dima <dmitryberesten@gmail.com>
2022-09-17 19:44:35 +0300

    Додав товари на сторінку

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -39,7 +39,7 @@ class Products {
             <li class="products-element">
                 <span class="products-element__name">${name}</span>
                 <img class="products-element__img" src="${img}" />
-                <span class="products-element__price">💰 ${price.toLocaleString()} USD</span>
+                <span class="products-element__price">💰 ${price.toLocaleString()} грн</span>
                 <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
                 ${activeText}
                 </button>
