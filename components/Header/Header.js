3170d474f64178f4a4ca833a9f203cbf380bbcfb
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-01-25 23:20:02 +0200

    Оновив посилання

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,22 +1,26 @@
 class Header {
 
-    // обробник подій який відкриває сторінку замовлень
-    handlerOpenShoppingPage() {
-        shoppingPage.render();
-    }
+  // обробник подій який відкриває сторінку замовлень
+  handlerOpenShoppingPage() {
+    shoppingPage.render();
+  }
 
-    // розмітка для сторінки
-    render(count) {
-        const html = `
-            <div class="header-container">
+  // розмітка для сторінки
+  render(count) {
+    const html = `
+           <div class="header-container">
 
            <div class="img-logo">
-            <img class="img-logo__image" src="img/favicon.png">
+             <a href="./main.html">
+             <img class="img-logo__image" src="img/favicon.png">
+             </a>
            </div>
 
             <div class="center-heading">
             
+            <a href="./main.html">
             <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Music Room</h1>
+            </a>
             </div>
 
             <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
@@ -26,15 +30,12 @@ class Header {
                 </svg>
                 </span>  ${count}</div>
             </div>
-        `
+        `;
 
-        // присвоєння шаблонної строки прямо на сторінку
-        ROOT_HEADER.innerHTML = html;
-    }
+    // присвоєння шаблонної строки прямо на сторінку
+    ROOT_HEADER.innerHTML = html;
+  }
 }
 
 // екземпляр класа
 const headerPage = new Header();
-
-
-
